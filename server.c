3dea155f4a39f4c8a9afe860314922c1e054b412
Ravi Desai <ravi@contactmonkey.com>
2026-07-07 05:22:13 -0400

    C server can now say hello-world
    Good milestone.

--- server.c
+++ server.c
@@ -49,71 +49,88 @@ handle_request(void* client_socket_ptr) {
 		// TODO response to client to gtfo
 	}
 
-	if (bytes_read > 0) {
-		printf("Received\n%s\n", buf);
-		printf("\ndone received\n");
-
-		// Get first line. Max 512B.
-		char* line_end = strstr(buf, "\r\n");
-		if (line_end == NULL) {
-			// TODO err to client. bad request headers
-		}
-		size_t line_len = line_end - buf;
-		if (line_len > 512) {
-			// TODO err. url too big. bad request
-		}
-		// TODO This should be an array that is global, and then write to the entry that is for
-		// this particular thread-index. This thread should know it's own index.
-		char* first_line = malloc(513);
-		first_line[0] = 0;
-		strncpy(first_line, buf, line_len);
-		first_line[line_len] = 0;
-		printf("First line:\n%s\n", first_line);
-
-		// GET or POST request?
-		// What is the endpoint integer?
-		// List all get params
-		// List all post params
-
-		// String split
-		char** strings = calloc(20, sizeof(char*));
-		int string_count = 0;
-		string_count = string_split(buf, strings, "\t", 20);
-
-		if (string_count < 2) {
-			char* errmsg;
-			asprintf(&errmsg, "Invalid string count:%d from string:%s\n", string_count, buf);
-			fprintf(stderr, "%s", errmsg);
-			dprintf(client_socket, "%s", errmsg);
-			free(errmsg);
-			close(client_socket);
-			return NULL;
-		}
-		char* first = strings[0];
-		char* second = strings[1];
-		int code = atoi(second);
-
-		char* resp = malloc(32);
-		switch (code) {
-			case 1:
-				resp = "foo";
-				break;
-			case 2:
-				resp = "bar";
-				break;
-			default:
-				resp = "baz";
-		}
-		printf("1: %s\n2: %s\nResult: %s\n", first, second, resp);
-
-		// Response
-		char* out = malloc(256);
-		out = resp;
-		write(client_socket, out, strlen(out));
+	if (bytes_read <= 0) {
+		close(client_socket);
+		return NULL;
 	}
 
-	close(client_socket);
-	return NULL;
+	printf("Received\n%s\n", buf);
+
+	// Get first line. Max 512B.
+	char* line_end = strstr(buf, "\r\n");
+	if (line_end == NULL) {
+		// TODO err to client. bad request headers
+	}
+	size_t line_len = line_end - buf;
+	if (line_len > 512) {
+		// TODO err. url too big. bad request
+	}
+	// TODO This should be an array that is global, and then write to the entry that is for
+	// this particular thread-index. This thread should know it's own index.
+	char* first_line = malloc(513);
+	first_line[0] = 0;
+	strncpy(first_line, buf, line_len);
+	first_line[line_len] = 0;
+	printf("First line:\n%s\n", first_line);
+
+	// Parse line-1
+	char* http_method = malloc(8); http_method[0]=0;
+	char* endpoint = malloc(256); endpoint[0]=0;
+	char* http_version = malloc(16); http_version[0]=0;
+	int sscanf_result = sscanf(first_line, "%7s %255s %15s", http_method, endpoint, http_version);
+	if (sscanf_result != 3) {
+		// TODO err bad request
+	}
+
+	// GET or POST request?
+	printf("HTTP METHOD: %s\n", http_method);
+
+	// What is the endpoint integer?
+	printf("Endpoint: %s\n", endpoint);
+
+	 char* response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+    write(client_socket, response, strlen(response));
+    return NULL;
+
+	// List all get params
+	// List all post params
+
+	// String split
+	char** strings = calloc(20, sizeof(char*));
+	int string_count = 0;
+	string_count = string_split(buf, strings, "\t", 20);
+
+	if (string_count < 2) {
+		char* errmsg;
+		asprintf(&errmsg, "Invalid string count:%d from string:%s\n", string_count, buf);
+		fprintf(stderr, "%s", errmsg);
+		dprintf(client_socket, "%s", errmsg);
+		free(errmsg);
+		close(client_socket);
+		return NULL;
+	}
+	char* first = strings[0];
+	char* second = strings[1];
+	int code = atoi(second);
+
+	char* resp = malloc(32);
+	switch (code) {
+		case 1:
+			resp = "foo";
+			break;
+		case 2:
+			resp = "bar";
+			break;
+		default:
+			resp = "baz";
+	}
+	printf("1: %s\n2: %s\nResult: %s\n", first, second, resp);
+
+	// Response
+	char* out = malloc(256);
+	out = resp;
+	write(client_socket, out, strlen(out));
+
 }
 
 int
